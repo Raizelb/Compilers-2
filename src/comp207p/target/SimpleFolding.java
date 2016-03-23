8cd3be65eae248797d487ce1bea1fc034c291c8d
Raizelb <gacon514@gmail.com>
2016-03-23 16:14:26 +0000

    kdjals

---
+++ src/comp207p/target/SimpleFolding.java
@@ -0,0 +1,12 @@
+/*
+ * Decompiled with CFR 0_114.
+ */
+package comp207p.target;
+
+import java.io.PrintStream;
+
+public class SimpleFolding {
+    public void simple() {
+        System.out.println(67 + 12345);
+    }
+}
